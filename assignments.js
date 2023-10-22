cc30dbadb1f7755e268ab1783e4ec6bec8c0d9b3
ABroadCoder <148570855+ABroadCoder@users.noreply.github.com>
2023-10-21 20:23:19 -0600

    Create assignments.js

---
+++ assignments.js
@@ -0,0 +1,12 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
