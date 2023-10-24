0e42e118cbf6d17b388033256b6e390c9dc26592
ABroadCoder <148570855+ABroadCoder@users.noreply.github.com>
2023-10-24 17:26:08 -0600

    Update assignments.js

--- assignments.js
+++ assignments.js
@@ -1,12 +1,63 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+const country = "The United States of America";
+const continent = "North America";
+let population = 330;
 
-console.log("Jonas");
-console.log(23);
+console.log(country);
+console.log(continent);
+console.log(population);
+/*
+//Datatypes Lecture
+//1.
+const isIsland = false;
+const language = 'English';
+//2.
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+let age = 30;
+age = 31;
 
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// const birthYear = 1991;
+// birthYear = 1990;
+
+//Basic Operators
+// 1.
+const halfCountryPopulation = population / 2;
+console.log(halfCountryPopulation);
+
+// 2.
+population++;
+console.log(population);
+
+// 3.
+const finlandPopulation = 6;
+console.log(population > finlandPopulation);
+
+// 4. 
+const averagePopulation = 33;
+console.log(population < averagePopulation);
+
+// 5. 
+let description = country + " is in " + continent + ", and its " + population + " people speak " + language;
+console.log(description);
+
+// Strings and Template Literals
+// 1. 
+description = `${country} is in ${continent}, and its ${population} people speak ${language}.`
+console.log(description);
+*/
+
+// Taking Decisions: if/else Statements
+//1. 
+if (population > 33) {
+    console.log(`${country}'s population is above average. 📈`)
+}
+else {
+    console.log(`${country}'s population is ${(33 - population)} million below average.`)
+};
+
+//2.
+// done above through changing variable value and observing results
+
