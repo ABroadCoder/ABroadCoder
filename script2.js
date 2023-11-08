e771ef7463d0911b50ebf6bf0fc0c92d732dadd5
ABroadCoder <148570855+ABroadCoder@users.noreply.github.com>
2023-11-07 22:03:06 -0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -269,8 +269,6 @@ console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his bes
 
 //Challenges help to solidify understanding of taught concepts.
 
-*/
-
 const jonas = {
     firstName: 'Jonas',
     lastName: 'Schmedtmann',
@@ -304,3 +302,54 @@ console.log(jonas.age);
 
 console.log(jonas.getSummary());
 
+
+// console.log('lifting weights repetition 1 🏋️‍♀️');
+
+//  for loop keeps running while condition is true
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`)
+};
+
+*/
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+    console.log(jonas[i]), typeof jonas[i];
+    // Filling an empty array
+    //types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
+};
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i <= years.length - 1; i++) {
+    ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// Continue and Break
+console.log('---ONLY STRINGS, USING CONTINUE---')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue;
+
+    console.log(jonas[i], typeof jonas[i]);
+};
+
+console.log('---BREAK WITH NUMBER---')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
+
+    console.log(jonas[i], typeof jonas[i]);
+};
