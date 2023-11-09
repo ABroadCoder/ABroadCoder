a5b14886f53205206b27a15b0f50a8cbcb193f45
ABroadCoder <148570855+ABroadCoder@users.noreply.github.com>
2023-11-09 15:04:55 -0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -310,7 +310,6 @@ for (let rep = 1; rep <= 10; rep++) {
     console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`)
 };
 
-*/
 
 const jonas = [
     'Jonas',
@@ -353,3 +352,49 @@ for (let i = 0; i < jonas.length; i++) {
 
     console.log(jonas[i], typeof jonas[i]);
 };
+
+//looping backward
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
+
+
+// loop within a loop
+
+for (let exercise = 1; exercise <= 3; exercise++) {
+    console.log(`--------Starting exercise ${exercise}`);
+
+    for (let rep = 1; rep <= 5; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}}`)
+    }
+}
+
+*/
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weight repetition ${rep}}`);
+// }
+
+let rep = 1;
+while (rep <= 10) {
+    //console.log(`WHILE: Lifting weight repetition ${rep}}`);
+    rep++;
+}
+
+let dice = (Math.trunc(Math.random() * 6)) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}.`);
+    dice = (Math.trunc(Math.random() * 6)) + 1;
+    if (dice === 6) console.log('Loop is about to end.')
+}
