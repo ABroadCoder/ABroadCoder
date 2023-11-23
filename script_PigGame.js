e2ddd8f35f5e9bae8dd323a336e3f9a3b1116045
ABroadCoder <148570855+ABroadCoder@users.noreply.github.com>
2023-11-22 21:59:10 -0700

    Update script_PigGame.js

--- script_PigGame.js
+++ script_PigGame.js
@@ -12,17 +12,30 @@ const buttonNew = document.querySelector('.btn--new');
 const buttonRoll = document.querySelector('.btn--roll');
 const buttonHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+
+// Starting conditions
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    diceEl.classList.add('hidden');
+    
+    const scores = [0, 0];
+    let currentScore = 0;
+    let activePlayer = 0;
+    let playing = true;
+
+// function to refactor code and avoid repetition...DRY code 😊
+const switchPlayer = function() {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 buttonRoll.addEventListener('click', function() {
+    if (playing) {
     // 1. Generate a random die roll
     const dice = Math.trunc(Math.random() * 6) +1;
     console.log(dice);
@@ -38,12 +51,51 @@ buttonRoll.addEventListener('click', function() {
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // Switch to other player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        currentScore = 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-    }   
+        switchPlayer();
+    }  
+}
 });   
 
+buttonHold.addEventListener('click', function() {
+    if (playing) {
+    console.log('Hold button pressed');
+    // 1. add current score to active player's total score
+    scores[activePlayer] += currentScore;
+    // e.g. scores[1] = scores[1] + currentScore;
+
+    // 2. display updated total score
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    // 3. check whether total score is at least 20
+    if (scores[activePlayer] >= 20) {
+        playing = false;
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        currentScore = 0;
+        diceEl.classList.add('hidden');
+    } else {
+        // switch player
+        switchPlayer();
+        currentScore = 0;
+    };  
+}
+})
+
+// New Game button functionality
+buttonNew.addEventListener('click', function() {
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    diceEl.classList.add('hidden');
+    
+    const scores = [0, 0];
+    let currentScore = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    let activePlayer = 0;
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    let playing = true;
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+});
 
