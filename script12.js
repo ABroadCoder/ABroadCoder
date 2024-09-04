7c1b2b2d52132076fddff2cb0ae9d632dbfe8d17
ABroadCoder <148570855+ABroadCoder@users.noreply.github.com>
2024-09-04 13:01:02 -0600

    Update script12.js

--- script12.js
+++ script12.js
@@ -22,8 +22,8 @@ const account1 = {
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2024-08-30T23:36:17.929Z',
+    '2024-09-02T12:01:20.894Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -43,7 +43,7 @@ const account2 = {
     '2020-02-05T16:33:06.386Z',
     '2020-04-10T14:43:26.374Z',
     '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2024-09-02T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -90,6 +90,25 @@ const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
   // .textContent = 0
 
+  // Dates generation
+  const formatMovementDate = function (date) {
+    const calcDaysPassed = (date1, date2) =>
+      Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+    const daysPassed = calcDaysPassed(new Date(), date);
+    console.log(daysPassed);
+
+    if (daysPassed === 0) return 'Today';
+    if (daysPassed === 1) return 'Yesterday';
+    if (daysPassed <= 7) return `${daysPassed} days ago`;
+    else {
+      const day = `${date.getDate()}`.padStart(2, 0);
+      const month = `${date.getMonth() + 1}`.padStart(2, 0);
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+    }
+  };
+
   // Sorting logic for movements table
   const movs = sort
     ? acc.movements.slice().sort((a, b) => a - b)
@@ -99,17 +118,14 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
     <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-    <div class="movements__date">${day}/${month}/${year}</div>
+    <div class="movements__date">${displayDate}</div>
           <div class="movements__value">${mov.toFixed(2)}€</div>
         </div>
     `;
@@ -336,6 +352,7 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+/*
 console.log(23 === 23.0);
 // Numbers stored in binary (base-2) format. This causes inaccuracies for numbers like 0.1, similar to inaccuracies when writing 1/3 in base-10 notation.
 
@@ -467,3 +484,16 @@ console.log(Date.now());
 
 future.setFullYear(2040);
 console.log(future);
+*/
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+
+console.log(days1);
+
+// for more precise date calculations, use a date library like moment.js
