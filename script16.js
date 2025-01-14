9f3d7c331ce37e4a80de17411c8ded6ef8cb90ea
ABroadCoder <148570855+ABroadCoder@users.noreply.github.com>
2025-01-14 04:27:22 -0700

    Update script16.js

--- script16.js
+++ script16.js
@@ -7,6 +7,7 @@ const countriesContainer = document.querySelector('.countries');
 // Function definitions
 
 const renderCountry = function (data, className = '') {
+  console.log(data);
   const html = `
         <article class="country ${className}" >
                 <img class="country__img" src="${data.flags.png}" />
@@ -26,9 +27,32 @@ const renderCountry = function (data, className = '') {
             </article>
         `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
+  // countriesContainer.style.opacity = 1;
 };
 
+countriesContainer.addEventListener('click', function () {
+  countriesContainer.style.display = 'none';
+});
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    console.log(response);
+    if (!response.ok) {
+      alert(
+        'Country name not recognized. Please enter the English name of a country.'
+      );
+      throw new Error(`${errorMsg} (${response.status})})`);
+    }
+    return response.json();
+  });
+};
+
+/*
 // const getCountryAndNeighbor = function (country) {
 //   // AJAX call country 1
 //   const request = new XMLHttpRequest();
@@ -69,22 +93,105 @@ const renderCountry = function (data, className = '') {
 
 // Promises: Escape "callback hell" by chaining--not nesting--promises
 
+// const getCountryData = function (country) {
+//   // Country 1 fetch request
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+//       if (!response.ok)
+//         throw new Error(
+//           `Country "${country}" not found (${response.status})})`
+//         );
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbor = data[0].borders[0];
+
+//       // Guard clause
+//       if (!neighbor) return;
+
+//       // Country 2 fetch request
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+//     })
+//     .then(response => {
+//       if (!response.ok)
+//         throw new Error(
+//           `Country "${country}" not found (${response.status})})`
+//         );
+//       response.json();
+//     })
+//     .then(data => renderCountry(data[0], 'neighbor'))
+//     .catch(err => {
+//       console.error(`${err} 🙉 🙉 🙉`);
+//       renderError(`Something went wrong 🙉 🙉 🙉 ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
 const getCountryData = function (country) {
+  let neighbor;
+
   // Country 1 fetch request
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
+  getJSON(
+    `https://restcountries.com/v3.1/name/${country}`,
+    `Country "${country}" not found. Please enter the English name of a country.`
+  )
     .then(data => {
       renderCountry(data[0]);
       const neighbor = data[0].borders[0];
-
-      // Guard clause
-      if (!neighbor) return;
+      console.log(data);
 
       // Country 2 fetch request
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbor}`,
+        `(Neighbor country not found)`
+      );
     })
-    .then(response => response.json())
-    .then(data => renderCountry(data[0], 'neighbor'));
+    .then(data => renderCountry(data[0], 'neighbor'))
+    .catch(err => {
+      console.error(`${err} 🙉 🙉 🙉`);
+      if (!neighbor) {
+        // alert('This country has no neighbors! (2)');
+      } else {
+        renderError(`Something went wrong 🙉 🙉 🙉 ${err.message}. Try again!`);
+      }
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
 };
 
-getCountryData('portugal');
+btn.addEventListener('click', function () {
+  const countryInput = prompt('Enter the name of a country (in English):');
+  if (countryInput !== null && countryInput.trim() !== '') {
+    getCountryData(`${countryInput}`);
+  } else if (countryInput === '') {
+    alert('Please enter a country name');
+  }
+});
+*/
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const whereAmI = async function (country) {
+  const {} = await getPosition();
+
+  const { latitude: lat, longitude: lng } = pos.coords;
+
+  return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+
+  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+  const data = await res.json();
+  console.log(data);
+  renderCountry(data[0]);
+};
+
+whereAmI('portugal');
+console.log('FIRST');
